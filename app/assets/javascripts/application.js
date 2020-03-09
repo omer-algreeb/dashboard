629bf95d8ffdaf9a91b48f2bb0e35ec093a36bc8
omer-algreeb <omer@jisr.net>
2020-03-09 16:53:41 +0300

    dashboard1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,27 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
 //
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+// require_tree .
+//= require core/jquery.min
+//= require core/popper.min
+//= require core/bootstrap-material-design.min
+//= require plugins/perfect-scrollbar.jquery.min
+//= require plugins/moment.min
+//= require plugins/sweetalert2
+//= require plugins/jquery.validate.min
+//= require plugins/jquery.bootstrap-wizard
+//= require plugins/bootstrap-selectpicker
+//= require plugins/bootstrap-datetimepicker.min
+//= require plugins/jquery.dataTables.min
+//= require plugins/bootstrap-tagsinput
+//= require plugins/jasny-bootstrap.min
+//= require plugins/fullcalendar.min
+//= require plugins/jquery-jvectormap
+//= require plugins/nouislider.min
+//= require plugins/chartist.min
+//= require plugins/bootstrap-notify
+//= require material-dashboard.js
+//= require demo
+
+
---
+++ app/helpers/dashboares_helper.rb
@@ -0,0 +1,2 @@
+module DashboaresHelper
+end
